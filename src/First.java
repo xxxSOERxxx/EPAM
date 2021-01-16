a3d0da0507221d9f999a6adb7763669c1d67e724
Eugene Piuta <soer.man@gmail.com>
2021-01-16 13:13:46 +0300

    first commit

---
+++ src/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 2;
+        int z = ((a - 3) * b/2) + c;
+        System.out.println(z);
+    }
+}
---
+++ src/Second.java
@@ -0,0 +1,6 @@
+public class Second {
+    public static void main(String[] args) {
+        double a = 1, b = 1, c = 1;
+        double x = ((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / 2 * a) - Math.pow(a, 2) *c + Math.pow(b, 2)-2;
+    }
+}
